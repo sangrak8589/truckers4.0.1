b1937add961db8aa3041b8aca04919a560511dd7
Ubuntu <ubuntu@ip-172-31-28-64.ec2.internal>
2020-03-02 04:16:04 +0000

    mongodb services added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 ﻿require('rootpath')();
 const express = require('express');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 
 // use JWT auth to secure the api
 app.use(jwt());
